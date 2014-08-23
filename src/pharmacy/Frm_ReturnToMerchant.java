0ab3419732915dfa6a8c5c4078855023e18ace44
yyergg <yyergg@gmail.com>
2014-08-23 23:43:13 +0800

    [In complete] Frm_ReturnToMerchant, Search.

--- src/pharmacy/Frm_ReturnToMerchant.java
+++ src/pharmacy/Frm_ReturnToMerchant.java
@@ -1,7 +1,9 @@
 package pharmacy;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,9 +12,11 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTable;
 
-import main.Frm_Main;
+import errormessage.StoredErrorMessage;
+import cc.johnwu.sql.DBC;
 import multilingual.Language;
 
+import java.awt.EventQueue;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
@@ -25,6 +29,9 @@ public class Frm_ReturnToMerchant extends JFrame {
 	private JTextField txt_Search;
 	private JTable tab_MedicineList;
 	private static final Language paragraph = Language.getInstance();
+	
+	StoredErrorMessage ErrorMessage = new StoredErrorMessage();
+
 	/**
 	 * Launch the application.
 	 */
@@ -51,12 +58,12 @@ public class Frm_ReturnToMerchant extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{685, 0};
-		gbl_contentPane.rowHeights = new int[]{47, 582, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 685, 0 };
+		gbl_contentPane.rowHeights = new int[] { 47, 582, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JPanel panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.weightx = 1.0;
@@ -67,12 +74,12 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_panel.gridy = 0;
 		contentPane.add(panel, gbc_panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{590, 97, 0};
-		gbl_panel.rowHeights = new int[]{37, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 590, 97, 0 };
+		gbl_panel.rowHeights = new int[] { 37, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		txt_Search = new JTextField();
 		GridBagConstraints gbc_txt_Search = new GridBagConstraints();
 		gbc_txt_Search.weightx = 0.7;
@@ -83,7 +90,7 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_txt_Search.gridy = 0;
 		panel.add(txt_Search, gbc_txt_Search);
 		txt_Search.setColumns(10);
-		
+
 		JButton btn_Search = new JButton(paragraph.getString("SEARCH"));
 		btn_Search.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -97,7 +104,7 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_btn_Search.gridx = 1;
 		gbc_btn_Search.gridy = 0;
 		panel.add(btn_Search, gbc_btn_Search);
-		
+
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.weightx = 1.0;
@@ -107,12 +114,12 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_panel_1.gridy = 1;
 		contentPane.add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{568, 87, 0};
-		gbl_panel_1.rowHeights = new int[]{515, 32, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[] { 568, 87, 0 };
+		gbl_panel_1.rowHeights = new int[] { 515, 32, 0 };
+		gbl_panel_1.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
 		panel_1.setLayout(gbl_panel_1);
-		
+
 		tab_MedicineList = new JTable();
 		GridBagConstraints gbc_tab_MedicineList = new GridBagConstraints();
 		gbc_tab_MedicineList.weightx = 1.0;
@@ -123,8 +130,9 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_tab_MedicineList.gridx = 0;
 		gbc_tab_MedicineList.gridy = 0;
 		panel_1.add(tab_MedicineList, gbc_tab_MedicineList);
-		
-		JButton btn_Return = new JButton(paragraph.getString("RETURNTOMERCHANT"));
+
+		JButton btn_Return = new JButton(
+				paragraph.getString("RETURNTOMERCHANT"));
 		btn_Return.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				btn_ReturnActionPerformed(e);
@@ -139,7 +147,7 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_btn_Return.gridx = 0;
 		gbc_btn_Return.gridy = 1;
 		panel_1.add(btn_Return, gbc_btn_Return);
-		
+
 		JButton btn_Close = new JButton(paragraph.getString("CLOSE"));
 		btn_Close.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -155,18 +163,51 @@ public class Frm_ReturnToMerchant extends JFrame {
 		gbc_btn_Close.gridy = 1;
 		panel_1.add(btn_Close, gbc_btn_Close);
 	}
-	
+
 	private void btn_SearchActionPerformed(java.awt.event.ActionEvent evt) {
-		String reg_guid="";
+		String reg_guid = txt_Search.getText();
 		showMedicineList(reg_guid);
 	}
+
 	private void btn_CloseActionPerformed(java.awt.event.ActionEvent evt) {
-		//todo close
+		// todo close
 	}
+
 	private void btn_ReturnActionPerformed(java.awt.event.ActionEvent evt) {
-		//todo return
+		// todo return
 	}
-	private void showMedicineList(String reg_guid){
-		
+
+	private void showMedicineList(String reg_guid) {
+		System.out.print(reg_guid);
+		String sqlMedicines = "SELECT medicines.code, medicines.item, medicine_stock.dosage, medicines.unit, medicine_stock.usage, "
+				+ "medicine_stock.way, medicine_stock.repeat_number, medicine_stock.quantity, medicine_stock.urgent, "
+				+ "medicine_stock.powder, medicine_stock.ps "
+				+ "FROM medicines, medicine_stock, outpatient_services, registration_info "
+				+ "WHERE registration_info.guid = '"
+				+ reg_guid
+				+ "' "
+				+ "AND medicine_stock.os_guid = outpatient_services.guid "
+				+ "AND outpatient_services.reg_guid = registration_info.guid "
+				+ "AND medicines.code = medicine_stock.m_code";
+		ResultSet rsMedicines = null;
+		try {
+			rsMedicines = DBC.executeQuery(sqlMedicines);
+			
+		} catch (SQLException e) {
+		} finally {
+			try {
+				DBC.closeConnection(rsMedicines);
+			} catch (SQLException e) {
+				ErrorMessage.setData(
+						"Pharmacy",
+						"Frm_ReturnToMerchant",
+						"print(Graphics g, PageFormat pf, int pageIndex)",
+						e.toString().substring(
+								e.toString().lastIndexOf(".") + 1,
+								e.toString().length()));
+				Logger.getLogger(RefreshPharmacy.class.getName()).log(
+						Level.SEVERE, null, e);
+			}
+		}
 	}
 }
