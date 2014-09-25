26eef53a094c4612de1579de562684aeca5343da
cash <cash924n@gmail.com>
2014-09-25 22:24:04 +0800

    [Feature] add logging mechanism

--- src/cc/johnwu/sql/DBC.java
+++ src/cc/johnwu/sql/DBC.java
@@ -13,9 +13,12 @@ import javax.swing.JOptionPane;
 
 import multilingual.Language;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.his.JPAUtil;
 import org.his.dao.SettingDao;
 import org.his.model.Setting;
+import org.his.util.CustomLogger;
 
 public class DBC {
 
@@ -32,6 +35,8 @@ public class DBC {
     private static ResultSet s_LocalRS;
 
     private static javax.swing.JFrame s_EnteredFrm;
+    
+    private static Logger logger = LogManager.getLogger(DBC.class.getName());
 
 
     static {    
@@ -172,97 +177,134 @@ public class DBC {
     }
 
     public static void closeConnection(Statement stmt) throws SQLException {
-        if(stmt==null) return ;
-        Connection conn = stmt.getConnection();
-        if(stmt!=null)stmt.close();
-        if(conn!=null)conn.close();
-    }
+        if(stmt==null) return;
+		Connection conn = stmt.getConnection();
+		if (stmt != null)
+			stmt.close();
+		if (conn != null)
+			conn.close();
+	}
 
-    public static void closeConnection(PreparedStatement pstmt) throws SQLException {
-        if(pstmt==null) return ;
-        Connection conn = pstmt.getConnection();
-        if(pstmt!=null)pstmt.close();
-        if(conn!=null)conn.close();
-    }
+	public static void closeConnection(PreparedStatement pstmt)
+			throws SQLException {
+		if (pstmt == null)
+			return;
+		Connection conn = pstmt.getConnection();
+		if (pstmt != null)
+			pstmt.close();
+		if (conn != null)
+			conn.close();
+	}
 
-    /** 執行給定的 SQL 語句，該語句返回單個 ResultSet 物件。*/
-    public static ResultSet executeQuery(String sql) throws SQLException {
-    	System.out.println(sql);
-        cc.johnwu.login.OnlineState.OnlineState();
-        Connection conn = DriverManager.getConnection(s_ServerURL,s_ServerName,s_ServerPasswd);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        return stmt.executeQuery(sql);
-    }
-    
-    public static Statement getQueryStatement() throws SQLException {
-        cc.johnwu.login.OnlineState.OnlineState();
-        Connection conn = DriverManager.getConnection(s_ServerURL,s_ServerName,s_ServerPasswd);
-        return conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-    }
+	/** 執行給定的 SQL 語句，該語句返回單個 ResultSet 物件。 */
+	public static ResultSet executeQuery(String sql) throws SQLException {
+		cc.johnwu.login.OnlineState.OnlineState();
+		Connection conn = DriverManager.getConnection(s_ServerURL,
+				s_ServerName, s_ServerPasswd);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		CustomLogger.debug(logger, sql);
+		return stmt.executeQuery(sql);
+	}
 
-    public static ResultSet executeQueryLogin(String sql) throws SQLException {
-        Connection conn = DriverManager.getConnection(s_ServerURL,s_ServerName,s_ServerPasswd);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        return stmt.executeQuery(sql);
-    }
+	public static Statement getQueryStatement() throws SQLException {
+		cc.johnwu.login.OnlineState.OnlineState();
+		Connection conn = DriverManager.getConnection(s_ServerURL,
+				s_ServerName, s_ServerPasswd);
+		return conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+	}
 
-    /** 執行給定 SQL 語句，該語句可能為 INSERT、UPDATE 或 DELETE 語句，或者不返回任何內容的 SQL 語句（如 SQL DDL 語句）。*/
-    public static int executeUpdate(String sql) throws SQLException {
-    	System.out.println(sql);
-        cc.johnwu.login.OnlineState.OnlineState();
-        int count = 0;
-        Connection conn = DriverManager.getConnection(s_ServerURL,s_ServerName,s_ServerPasswd);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        count = stmt.executeUpdate(sql);
-        closeConnection(stmt);
-        return count;
-    }
+	public static ResultSet executeQueryLogin(String sql) throws SQLException {
+		Connection conn = DriverManager.getConnection(s_ServerURL,
+				s_ServerName, s_ServerPasswd);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		CustomLogger.debug(logger, sql);
+		return stmt.executeQuery(sql);
+	}
 
-    public static int executeUpdateLogin(String sql) throws SQLException {
-        int count = 0;
-        Connection conn = DriverManager.getConnection(s_ServerURL,s_ServerName,s_ServerPasswd);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        count = stmt.executeUpdate(sql);
-        closeConnection(stmt);
-        return count;
-    }
+	/**
+	 * 執行給定 SQL 語句，該語句可能為 INSERT、UPDATE 或 DELETE 語句，或者不返回任何內容的 SQL 語句（如 SQL DDL
+	 * 語句）。
+	 */
+	public static int executeUpdate(String sql) throws SQLException {
+		cc.johnwu.login.OnlineState.OnlineState();
+		int count = 0;
+		Connection conn = DriverManager.getConnection(s_ServerURL,
+				s_ServerName, s_ServerPasswd);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		count = stmt.executeUpdate(sql);
+		closeConnection(stmt);
+		CustomLogger.debug(logger, sql);
+		return count;
+	}
 
-    public static PreparedStatement prepareStatement(String sql) throws SQLException {
-        Connection conn = DriverManager.getConnection(s_ServerURL,s_ServerName,s_ServerPasswd);
-        return conn.prepareStatement(sql);
-    }
+	public static int executeUpdateLogin(String sql) throws SQLException {
+		int count = 0;
+		Connection conn = DriverManager.getConnection(s_ServerURL,
+				s_ServerName, s_ServerPasswd);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		count = stmt.executeUpdate(sql);
+		closeConnection(stmt);
+		CustomLogger.debug(logger, sql);
+		return count;
+	}
 
-    /** 執行給定的 SQL 語句，該語句返回單個 ResultSet 物件。*/
-    public synchronized static ResultSet localExecuteQuery(String sql) throws SQLException {
-        Connection conn = DriverManager.getConnection(LOCALURL,LOCALNAME,LOCALPASSWD);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        System.out.println("[LOCAL]: " + sql);
-        return stmt.executeQuery(sql);
-    }
+	public static PreparedStatement prepareStatement(String sql)
+			throws SQLException {
+		Connection conn = DriverManager.getConnection(s_ServerURL,
+				s_ServerName, s_ServerPasswd);
+		return conn.prepareStatement(sql);
+	}
 
-    /** 執行給定 SQL 語句，該語句可能為 INSERT、UPDATE 或 DELETE 語句，或者不返回任何內容的 SQL 語句（如 SQL DDL 語句）。*/
-    public static int localExecuteUpdate(String sql) throws SQLException {
-        int count = 0;
-        Connection conn = DriverManager.getConnection(LOCALURL,LOCALNAME,LOCALPASSWD);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        count = stmt.executeUpdate(sql);
-        closeConnection(stmt);
-        System.out.println("[LOCAL]: " + sql);
-        return count;
-    }
+	/** 執行給定的 SQL 語句，該語句返回單個 ResultSet 物件。 */
+	public synchronized static ResultSet localExecuteQuery(String sql)
+			throws SQLException {
+		Connection conn = DriverManager.getConnection(LOCALURL, LOCALNAME,
+				LOCALPASSWD);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		CustomLogger.debug(logger, "[LocalDB] {}", sql);
+		return stmt.executeQuery(sql);
+	}
 
-    public synchronized static boolean localExecute(String sql) throws SQLException {
-        boolean flag = false;
-        Connection conn = DriverManager.getConnection(LOCALURL,LOCALNAME,LOCALPASSWD);
-        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        flag = stmt.execute(sql);
-        closeConnection(stmt);
-        return flag;
-    }
+	/**
+	 * 執行給定 SQL 語句，該語句可能為 INSERT、UPDATE 或 DELETE 語句，或者不返回任何內容的 SQL 語句（如 SQL DDL
+	 * 語句）。
+	 */
+	public static int localExecuteUpdate(String sql) throws SQLException {
+		int count = 0;
+		Connection conn = DriverManager.getConnection(LOCALURL, LOCALNAME,
+				LOCALPASSWD);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		count = stmt.executeUpdate(sql);
+		closeConnection(stmt);
+		CustomLogger.debug(logger, "[LocalDB] {}", sql);
+		return count;
+	}
 
-    public synchronized static PreparedStatement localPrepareStatement(String sql) throws SQLException {
-        Connection conn = DriverManager.getConnection(LOCALURL,LOCALNAME,LOCALPASSWD);
-        return conn.prepareStatement(sql);
-    }
+	public synchronized static boolean localExecute(String sql)
+			throws SQLException {
+		boolean flag = false;
+		Connection conn = DriverManager.getConnection(LOCALURL, LOCALNAME,
+				LOCALPASSWD);
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		flag = stmt.execute(sql);
+		closeConnection(stmt);
+		CustomLogger.debug(logger, "[LocalDB] {}", sql);
+		return flag;
+	}
+
+	public synchronized static PreparedStatement localPrepareStatement(
+			String sql) throws SQLException {
+		Connection conn = DriverManager.getConnection(LOCALURL, LOCALNAME,
+				LOCALPASSWD);
+		return conn.prepareStatement(sql);
+	}
 
 }
