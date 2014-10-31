77231d3a6a1a867cc72ac614e628bfa48418aa5c
mamafun <mamafun@gmail.com>
2014-10-31 09:04:30 +0800

    [incomplete] need to import zxing library

---
+++ src/camera/Frm_qrcode.java
@@ -0,0 +1,98 @@
+package camera;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Frm_qrcode extends JFrame implements Runnable, ThreadFactory {
+
+	private static final long serialVersionUID = 6441489157408381878L;
+
+	private Executor executor = Executors.newSingleThreadExecutor(this);
+
+	private Webcam webcam = null;
+	private WebcamPanel panel = null;
+	private JTextArea textarea = null;
+
+	public Frm_qrcode() {
+		super();
+
+		setLayout(new FlowLayout());
+		setTitle("Read QR / Bar Code With Webcam");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Dimension size = WebcamResolution.QVGA.getSize();
+
+		webcam = Webcam.getWebcams().get(0);
+		webcam.setViewSize(size);
+
+		panel = new WebcamPanel(webcam);
+		panel.setPreferredSize(size);
+
+		textarea = new JTextArea();
+		textarea.setEditable(false);
+		textarea.setPreferredSize(size);
+
+		add(panel);
+		add(textarea);
+
+		pack();
+		setVisible(true);
+
+		executor.execute(this);
+	}
+
+	@Override
+	public void run() {
+
+		do {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			Result result = null;
+			BufferedImage image = null;
+
+			if (webcam.isOpen()) {
+
+				if ((image = webcam.getImage()) == null) {
+					continue;
+				}
+
+				LuminanceSource source = new BufferedImageLuminanceSource(image);
+				BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+				try {
+					result = new MultiFormatReader().decode(bitmap);
+				} catch (NotFoundException e) {
+					// fall thru, it means there is no QR code in image
+				}
+			}
+
+			if (result != null) {
+				textarea.setText(result.getText());
+			}
+
+		} while (true);
+	}
+
+	@Override
+	public Thread newThread(Runnable r) {
+		Thread t = new Thread(r, "example-runner");
+		t.setDaemon(true);
+		return t;
+	}
+
+	public static void main(String[] args) {
+		new WebcamQRCodeExample();
+	}
+
+}
